e3dfddfe6a8ab1724179b319b77068a93debbf26
vinco <zappeln@gmail.com>
2014-04-06 16:03:18 -0500

    brand seed and index listed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Brand.create(name: "Brothers", store: "Publix")
+
+Brand.create(name: "Eight O'Clock", store: "supermarkets")
+
+Brand.create(name: "Starbucks", store: "supermarkets")
+
+Brand.create(name: "Dunkin'", store: "supermarkets")
+
+Brand.create(name: "Green Mountain", store: "supermarkets")
+
+Brand.create(name: "Peet's", store: "supermarkets")
