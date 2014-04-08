749cc2421c69f79b071ffbec211efe47f567bf87
vinco <zappeln@gmail.com>
2014-04-08 18:12:32 -0500

    bean avg scores

--- app/models/bean.rb
+++ app/models/bean.rb
@@ -1,4 +1,12 @@
 class Bean < ActiveRecord::Base
   belongs_to :brand
   has_many :reviews
+
+  def avg_score
+    score = []
+    Review.where(bean_id: self.id).each{ |review| score << review.rating }
+    return 0 if score.length == 0
+    divisor = score.length
+    score.reduce(:+) / divisor
+  end
 end
