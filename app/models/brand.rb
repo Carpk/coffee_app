9cc5ff3b0e1666c531a9711ef8f95f562a490553
vinco <zappeln@gmail.com>
2014-04-08 18:27:10 -0500

    scores on all pages

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,3 +1,11 @@
 class Brand < ActiveRecord::Base
   has_many :beans
+
+  def avg_score
+    score = []
+    Bean.where(brand_id: self.id).each{ |bean| score << bean.avg_score }
+    return 0 if score.length == 0
+    divisor = score.length
+    score.reduce(:+) / divisor
+  end
 end
