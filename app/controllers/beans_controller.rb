6822135dc2ca19fa79a62b138745a5b5b59f510f
vinco <zappeln@gmail.com>
2014-04-09 09:24:46 -0500

    forms for all fields

--- app/controllers/beans_controller.rb
+++ app/controllers/beans_controller.rb
@@ -1,7 +1,12 @@
 class BeansController < ApplicationController
 
+  def create
+    
+  end
+
   def show
     @reviews = Review.where(bean_id: params[:brand_id])
     @bean = Bean.find_by(id: params[:brand_id])
+    @new_review = Review.new
   end
 end
--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,12 +1,17 @@
 class BrandsController < ApplicationController
   
+  def create
+    
+  end
+  
   def index
     @brands = Brand.all
+    @new_brand = Brand.new
   end
 
   def show
     @beans = Bean.where(brand_id: params[:id])
     @brand = Brand.find_by(id: params[:id])
-    
+    @new_bean = Bean.new
   end
 end
